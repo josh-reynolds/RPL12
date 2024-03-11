df81045854aa590248cbac631fe9ee2940e90752
josh <josh.reynolds@frontier.com>
2024-03-11 12:04:32 -0700

    RPL Chapter 12 - handling arg parsing errors

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -20,10 +24,15 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    // does not handle if not enough args are passed
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+  fn build(args: &[String]) -> Result<Config, &'static str> {
+      if args.len() < 3 {
+          return Err("not enough arguments");
+      }
 
-    Config { query, file_path }
+      let query = args[1].clone();
+      let file_path = args[2].clone();
+
+      Ok(Config { query, file_path })
+  }
 }
