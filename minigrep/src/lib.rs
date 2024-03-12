8cba67dc6c535fd843c13afde71529897e407e7e
josh <josh.reynolds@frontier.com>
2024-03-11 20:42:39 -0700

    RPL Chapter 12 - moving code to library module

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,9 +1,27 @@
+use std::fs;
+use std::error::Error;
 
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    // does not handle if not enough args are passed
-    let query = &args[1];
-    let file_path = &args[2];
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
 
-    (query, file_path)
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+    Ok(())
 }
